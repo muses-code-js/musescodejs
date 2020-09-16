f10f24db9bfec442846a510c8f49535c26b18eab
tandrade <tandrade@atlassian.com>
2020-09-16 18:22:37 +1000

    fix after review

--- site/graphql/resources.js
+++ site/graphql/resources.js
@@ -1,15 +1,5 @@
 import gql from 'graphql-tag';
 
-export const RESOURCE_DATA = gql`
-    fragment ResourceData on Resource{
-        id
-        title
-        topic
-        level
-        url
-    }
-`;
-
 export const GET_ALL_RESOURCES = gql`
   query {
     allResources {
--- site/pages/resources.js
+++ site/pages/resources.js
@@ -1,46 +1,45 @@
 /** @jsx jsx */
 import { jsx } from '@emotion/core';
 import { useQuery } from '@apollo/react-hooks';
-import { GET_ALL_RESOURCES } from '../graphql/resources'
+import { GET_ALL_RESOURCES } from '../graphql/resources';
 
 import ResourceItem from '../components/ResourcesItems/ResourceItem';
 import SearchBar from '../components/SearchBar';
-import { Container, Loading, H2, H3 } from '../primitives';
+import { Container, Loading, H2 } from '../primitives';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import Meta from '../components/Meta';
 import { colors, gridSize } from '../theme';
 
- 
-function Resources(){
-    const { data, loading, error } = useQuery(GET_ALL_RESOURCES);
-    
-    if (error) {
-        console.error('Failed to load resources', error);
-    }
+function Resources() {
+  const { data, loading, error } = useQuery(GET_ALL_RESOURCES);
 
-    return (
-       <>
-           <Meta title="Events" />
-           <Navbar background="white" />
-           <Container css={{ marginTop: gridSize * 3 }}>
-                <div css={{ display: 'flex', justifyContent: 'space-between'}}>
-                    <H2 hasSeparator css={{color: `${colors.pink}`, marginBottom: `${gridSize *7}px`}}>Resources</H2>
-                    <SearchBar placeholder="Search"  />
-                </div>
-            {loading ? (
-                <Loading isCentered size="xlarge" />
-            ) : error ? (
-                <p>Something went wrong. Please try again.</p>
-            ) : (
-                    <ResourceItem resources={data.allResources} />
-            )}
+  if (error) {
+    console.error('Failed to load resources', error);
+  }
 
-           </Container>
-           <Footer /> 
-       </>
-     
-   );
+  return (
+    <>
+      <Meta title="Events" />
+      <Navbar background="white" />
+      <Container css={{ marginTop: gridSize * 3 }}>
+        <div css={{ display: 'flex', justifyContent: 'space-between' }}>
+          <H2 hasSeparator css={{ color: `${colors.pink}`, marginBottom: `${gridSize * 7}px` }}>
+            Resources
+          </H2>
+          <SearchBar placeholder="Search" />
+        </div>
+        {loading ? (
+          <Loading isCentered size="xlarge" />
+        ) : error ? (
+          <p>Something went wrong. Please try again.</p>
+        ) : (
+          <ResourceItem resources={data.allResources} />
+        )}
+      </Container>
+      <Footer />
+    </>
+  );
 }
- 
+
 export default Resources;
